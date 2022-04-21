cc49f35c6bffd7abab47ac1feebee46e82ad27c5
fe-zhang <yulinzhang001@qq.com>
2022-04-21 22:19:55 +0800

    一些优化

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -15,7 +15,7 @@ const HeadView: React.FC<IProps> = (props) => {
     } = props;
     return (
         <Head>
-            <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+            <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
             <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
             <meta content="yes" name="apple-mobile-web-app-capable" />
             <meta content="telephone=no" name="format-detection" />
